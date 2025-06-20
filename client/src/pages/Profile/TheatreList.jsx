a439b333496c88f87b7e8f4e271c3a47bfadf5b0
Achuth B <achutampi19@gmail.com>
2025-06-20 23:02:43 +0530

    form

--- client/src/pages/Profile/TheatreList.jsx
+++ client/src/pages/Profile/TheatreList.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "antd";
+import "./TheatreList.css";
+import TheatreFormModal from "./theatreForm";
 
 function TheatreList() {
-  return <div>THerare list</div>;
+  const [isModelOpen, setIsModelOpen] = useState(false);
+  return (
+    <>
+      <div className="flex justify-end mx-0.7 ">
+        <Button
+          onClick={() => {
+            setIsModelOpen(true);
+          }}
+          className="my-button"
+        >
+          Add Theatre
+        </Button>
+        {isModelOpen && (
+          <TheatreFormModal
+            isModelOpen={isModelOpen}
+            setIsModelOpen={setIsModelOpen}
+          />
+        )}
+      </div>
+    </>
+  );
 }
 
 export default TheatreList;
