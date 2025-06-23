032c09fdde6dc42b83e536b98a7597d36733153e
Achuth B <achutampi19@gmail.com>
2025-06-23 12:41:46 +0530

    error correction

--- client/src/pages/singleMovie.jsx
+++ client/src/pages/singleMovie.jsx
@@ -2,17 +2,17 @@ import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { getMovieById } from "../apicalls/movies";
 import { useDispatch } from "react-redux";
-import { showLoading, hideLoading } from "../redux/loaderSlice";
+import { hideLoading, showLoading } from "../redux/loaderSlice";
 import { message, Input, Divider, Row, Col } from "antd";
 import { CalendarOutlined } from "@ant-design/icons";
 import moment from "moment";
-//import { getAllTheatresByMovie } from "../apicalls/shows";
+import { getAllTheatresByMovie } from "../apicalls/shows";
 
 const SingleMovie = () => {
   const params = useParams();
   const [movie, setMovie] = useState();
   const [date, setDate] = useState(moment().format("YYYY-MM-DD"));
-  //const [theatres, setTheatres] = useState([]);
+  const [theatres, setTheatres] = useState([]);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const handleDate = (e) => {
@@ -36,41 +36,41 @@ const SingleMovie = () => {
     }
   };
 
-  // const getAllTheatres = async () => {
-  //   try {
-  //     dispatch(showLoading());
-  //     const response = await getAllTheatresByMovie({ movie: params.id, date });
-  //     if (response.success) {
-  //       setTheatres(response.data);
-  //       console.log(theatres)
-  //     } else {
-  //       message.error(response.message);
-  //     }
-  //     dispatch(hideLoading());
-  //   } catch (err) {
-  //     dispatch(hideLoading());
-  //     message.err(err.message);
-  //   }
-  // };
+  const getAllTheatres = async () => {
+    try {
+      dispatch(showLoading());
+      const response = await getAllTheatresByMovie({ movie: params.id, date });
+      if (response.success) {
+        setTheatres(response.data);
+        console.log(theatres);
+      } else {
+        message.error(response.message);
+      }
+      dispatch(hideLoading());
+    } catch (err) {
+      dispatch(hideLoading());
+      message.err(err.message);
+    }
+  };
 
   useEffect(() => {
     getData();
   }, []);
 
-  // useEffect(() => {
-  //   getAllTheatres();
-  // }, [date]);
+  useEffect(() => {
+    getAllTheatres();
+  }, [date]);
 
   return (
     <>
-      <div className="inner-container">
+      <div className="max-w-[1000px] mx-auto">
         {movie && (
-          <div className="shrink-0 me-3 mb-4">
-            <div className="flex flex-col">
+          <div className="flex flex-col md:flex-row">
+            <div className="flex-shrink-0 mb-4 md:mb-0 md:mr-4">
               <img src={movie.poster} width={150} alt="Movie Poster" />
             </div>
             <div className="w-full">
-              <h1 className="mt-0">{movie.title}</h1>
+              <h1 className="mt-0 text-2xl font-semibold">{movie.title}</h1>
               <p className="text-gray-500 text-sm mt-1 mb-2">
                 Language:{" "}
                 <span className="text-gray-900 font-semibold">
@@ -95,23 +95,71 @@ const SingleMovie = () => {
                   {movie.duration} Minutes
                 </span>
               </p>
-
-              <hr />
-              {
-                <div className="d-flex flex-column-mob align-items-center mt-3">
-                  <label className="me-3 flex-shrink-0">Choose the date:</label>
-                  <Input
-                    onChange={handleDate}
-                    type="date"
-                    min={moment().format("YYYY-MM-DD")}
-                    className="max-w-[300px] mt-2 sm:mt-0"
-                    value={date}
-                    placeholder="default size"
-                    prefix={<CalendarOutlined />}
-                  />
-                </div>
-              }
+              <hr className="border border-gray-200 my-4" />
+              <div className="flex flex-col md:flex-row items-center mt-4">
+                <label className="md:mr-4 flex-shrink-0 mb-2 md:mb-0">
+                  Choose the date:
+                </label>
+                <Input
+                  onChange={handleDate}
+                  type="date"
+                  min={moment().format("YYYY-MM-DD")}
+                  className="max-w-[300px] mt-2 md:mt-0"
+                  value={date}
+                  placeholder="default size"
+                  prefix={<CalendarOutlined />}
+                />
+              </div>
             </div>
+          </div>
+        )}
+        {theatres.length === 0 && (
+          <div className="pt-4">
+            <h2 className="text-[#1890ff] text-lg font-semibold">
+              Currently, no theatres available for this movie!
+            </h2>
+          </div>
+        )}
+        {theatres.length > 0 && (
+          <div className="mt-4 pt-4">
+            <h2 className="text-xl font-semibold mb-4">Theatres</h2>
+            {theatres.map((theatre) => {
+              return (
+                <div key={theatre._id}>
+                  <Row gutter={24} key={theatre._id}>
+                    <Col xs={{ span: 24 }} lg={{ span: 8 }}>
+                      <h3 className="text-lg font-semibold">{theatre.name}</h3>
+                      <p className="text-sm text-gray-600">{theatre.address}</p>
+                    </Col>
+                    <Col xs={{ span: 24 }} lg={{ span: 16 }}>
+                      <ul className="flex flex-wrap gap-2 p-0 list-none">
+                        {theatre.shows
+                          .sort(
+                            (a, b) =>
+                              moment(a.time, "HH:mm") - moment(b.time, "HH:mm")
+                          )
+                          .map((singleShow) => {
+                            return (
+                              <li
+                                key={singleShow._id}
+                                onClick={() =>
+                                  navigate(`/book-show/${singleShow._id}`)
+                                }
+                                className="px-4 py-3 border border-gray-400 rounded cursor-pointer text-orange-500 hover:text-white hover:bg-orange-500 hover:border-transparent transition-all"
+                              >
+                                {moment(singleShow.time, "HH:mm").format(
+                                  "hh:mm A"
+                                )}
+                              </li>
+                            );
+                          })}
+                      </ul>
+                    </Col>
+                  </Row>
+                  <hr className="border border-gray-200 my-4" />
+                </div>
+              );
+            })}
           </div>
         )}
       </div>
